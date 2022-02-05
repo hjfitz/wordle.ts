98571887ff68f05326b38d8cefe02157f0fec361
Harry Fitzgerald <hjfitz@protonmail.ch>
2022-02-05 16:55:19 +0000

    add pattern matching to cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,8 @@
 import { Command } from "commander";
 import config from "./config";
-import {fetchWordList} from "./fetch-word-list";
+import {fetchWordList, WordList} from "./fetch-word-list";
 import {findMatchingWords} from "./find-matching-words";
+import {matchPattern} from './match-pattern'
 
 const prog = new Command()
 
@@ -15,6 +16,7 @@ const out = (words: string) => console.log(`
 
 prog
 	.option('-c, --chars <characters>', 'The list of characters you have in any order')
+	.option('-p, --pattern <pattern>', 'a pattern to match, if you know the positions. Eg: al**t')
 	.option('-l, --list <word-list-url>', 'a link to a plain text list of words split by newline. Optional')
 
 async function main() {
@@ -22,11 +24,11 @@ async function main() {
 
 	let wordListUrl = config.word_list_url
 
-	const {chars, list} = prog.opts()
+	const {chars, list, pattern} = prog.opts()
 
 	if (list) wordListUrl = list
 
-	if (!chars) {
+	if (!chars && !pattern) {
 		console.error(prog.helpInformation())
 		process.exit(1)
 	}
@@ -38,7 +40,15 @@ async function main() {
 
 	const wordList = await fetchWordList(wordListUrl)
 
-	const matches = findMatchingWords(chars.toLowerCase(), wordList)
+	let matches: WordList = []
+
+	if (chars) {
+		matches = findMatchingWords(chars.toLowerCase(), wordList)
+	}
+
+	if (pattern) {
+		matches = matchPattern(pattern, matches)
+	}
 
 	console.log('\n')
 	console.log('Possible words:')
